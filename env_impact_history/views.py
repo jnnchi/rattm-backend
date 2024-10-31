9ea1253200f75c64246655592296c17181e2d904
Chong <chong.wan@mail.utoronto.ca>
2024-10-31 17:13:47 -0400

    commented out unused functions

--- env_impact_history/views.py
+++ env_impact_history/views.py
@@ -4,12 +4,12 @@ from .models import EnvImpactHistory
 from .serializers import EnvImpactHistorySerializer
 from entity import get_score, get_total_green_transactions, get_most_purchased_companies, get_ESG_score_of_transaction_companies
 # Here we do the data structure conversion for the entity.py functions
-def get_score(request):
+# def get_score(request):
 
-def get_ESG_score_of_transaction_companies(request):
+# def get_ESG_score_of_transaction_companies(request):
     
-def get_total_green_transactions(request):
+# def get_total_green_transactions(request):
 
-def get_most_purchased_companies(request):
+# def get_most_purchased_companies(request):
     
     
