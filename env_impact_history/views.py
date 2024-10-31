1d8c35ca14278a8c1bc735a79acf9bc81ca8568e
Chong <chong.wan@mail.utoronto.ca>
2024-10-31 17:29:44 -0400

    fixed import statement

--- env_impact_history/views.py
+++ env_impact_history/views.py
@@ -2,7 +2,7 @@
 from rest_framework import viewsets
 from .models import EnvImpactHistory
 from .serializers import EnvImpactHistorySerializer
-from entity import get_score, get_total_green_transactions, get_most_purchased_companies, get_ESG_score_of_transaction_companies
+from .entity import get_score, get_total_green_transactions, get_most_purchased_companies, get_ESG_score_of_transaction_companies
 # Here we do the data structure conversion for the entity.py functions
 # def get_score(request):
 
