7d68917783485c5e777680f539cfd56be6cfcf10
Chong <chong.wan@mail.utoronto.ca>
2024-11-01 17:59:28 -0400

    modified file name for CA

--- env_impact_history/urls.py
+++ env_impact_history/urls.py
@@ -3,9 +3,6 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import EnvImpactHistory
 
-router = DefaultRouter()
-router.register(r'env_impact_history', EnvImpactHistory)
-
 urlpatterns = [
     path('get_scr', get_score, name='get_score'),
     path('get_green_transact', get_total_green_transactions, name='get_green_transact'),
