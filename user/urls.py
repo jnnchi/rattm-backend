2d763f6af1d4761eabbd3e7e42cc29a4374f22df
Jennifer Chiou <jennifer.r.chiou@gmail.com>
2024-11-01 21:50:20 -0400

    Update urls.py

--- user/urls.py
+++ user/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import get_user_ip_from_frontend
+from .views import get_user_email_from_frontend
 
 urlpatterns = [
     path('get_email/', get_user_email_from_frontend, name='get_user_email')
