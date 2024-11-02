af21ec193c4313f762f0a40c92e10bfc5a60e984
Jennifer Chiou <jennifer.r.chiou@gmail.com>
2024-11-01 21:49:44 -0400

    changed ip to email

--- user/urls.py
+++ user/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import get_user_ip_from_frontend
 
 urlpatterns = [
-    path('get_ip/', get_user_ip_from_frontend, name='get_user_ip')
+    path('get_email/', get_user_email_from_frontend, name='get_user_email')
 ]
--- user/views.py
+++ user/views.py
@@ -8,7 +8,7 @@ from rest_framework.response import Response
 # def create_user(request):
 
 @api_view('POST')
-def get_user_ip_from_frontend(request):
+def get_user_email_from_frontend(request):
     print(request.data)
     return Response({"message": "Got user's email", "data": request.data})
     
